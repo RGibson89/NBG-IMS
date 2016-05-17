15483ba4df12c258191d21ba0a5eaafdb1ab524c
Robbie <r.gibbon@yahoo.co.uk>
2016-05-17 10:42:32 +0100

    init project

---
+++ nbgardens-ims/src/main/java/com/qa/nbgardens_ims/App.java
@@ -0,0 +1,13 @@
+package com.qa.nbgardens_ims;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
