5f4588752c7f32df76828f19c8771a9f264e1678
mike <mikebourke@email.com>
2016-05-19 15:17:15 +0000

    added product class

--- workspace/NBGardens/src/Product.java
+++ workspace/NBGardens/src/Product.java
@@ -1,11 +1,23 @@
-
 public class Product {
 String name;
 String description;
+int productID;
 int quantityInStock;
 double weight;
 double price;
 
+
+
+
+public Product(String name, String description, int productID, int quantityInStock, double weight, double price) 
+{
+	this.name = name;
+	this.description = description;
+	this.productID = productID;
+	this.quantityInStock = quantityInStock;
+	this.weight = weight;
+	this.price = price;
+}
 
 
 
@@ -14,32 +26,20 @@ double price;
 public String getName() {
 	return name;
 }
-public void setName(String name) {
-	this.name = name;
-}
 public String getDescription() {
 	return description;
 }
-public void setDescription(String description) {
-	this.description = description;
+public int getproductID() {
+	return productID;
 }
 public int getQuantityInStock() {
 	return quantityInStock;
 }
-public void setQuantityInStock(int quantityInStock) {
-	this.quantityInStock = quantityInStock;
-}
 public double getWeight() {
 	return weight;
 }
-public void setWeight(double weight) {
-	this.weight = weight;
-}
 public double getPrice() {
 	return price;
-}
-public void setPrice(double price) {
-	this.price = price;
 }
 
 }
