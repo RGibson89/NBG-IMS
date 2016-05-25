3b9e707f24f5909218c357ad6047987625f66ca0
DanArmstrong94 <danielcarmstrong@hotmail.co.uk>
2016-05-25 09:30:17 +0000

    further cleanup

--- workspace/NBGardens/src/main/CustomerController.java
+++ workspace/NBGardens/src/main/CustomerController.java
@@ -1,4 +1,4 @@
-package src.main;
+package main;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
--- workspace/NBGardens/src/main/Product.java
+++ workspace/NBGardens/src/main/Product.java
@@ -1,4 +1,4 @@
-package src.main;
+package main;
 public class Product {
 	int productID;
 	String name;
