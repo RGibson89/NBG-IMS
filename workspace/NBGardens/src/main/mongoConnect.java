d02c84ecfa36974947b2a45dfcf881d8939f6137
mike <mikebourke@email.com>
2016-05-26 13:21:03 +0000

    updated mongoConnect

--- workspace/NBGardens/src/main/mongoConnect.java
+++ workspace/NBGardens/src/main/mongoConnect.java
@@ -1,5 +1,39 @@
 package main;
 
+import com.mongodb.*;
+
+
+
 public class mongoConnect {
 
+	
+	MongoClient mongoClient = new MongoClient( "localhost" , 27017 );
+
+	
+	
+	
+	
+	
+	
+	/*
+	
+	System.out.println("			   	        _._                             ");                     
+	System.out.println("                                   _.-``__ ''-._                   ");                          
+	System.out.println("                              _.-``    `.  `_.  ''-._           ");
+	System.out.println("			  .-`` .-```.  ```/    _.,_ ''-._         ");                          
+	System.out.println("			 (    '      ,       .-`  | `,    )     ");
+	System.out.println("			 |`-._`-...-` __...-.``-._|'` _.-'|     ");
+	System.out.println("			 |    `-._   `._    /     _.-'    |     ");
+	System.out.println("			  `-._    `-._  `-./  _.-'    _.-'                    ");               
+	System.out.println("			 |`-._`-._    `-.__.-'    _.-'_.-'|                ");                  
+	System.out.println("			 |    `-._`-._        _.-'_.-'    |             ");      
+	System.out.println("			  `-._    `-._`-.__.-'_.-'    _.-'          ");                        
+	System.out.println("			 |`-._`-._    `-.__.-'    _.-'_.-'|       ");                           
+	System.out.println("			 |    `-._`-._        _.-'_.-'    |    ");                              
+	System.out.println("			  `-._    `-._`-.__.-'_.-'    _.-'  ");                                 
+	System.out.println("	                      `-._    `-.__.-'    _.-'   ");                                    
+	System.out.println("				  `-._        _.-'    ");                                       
+	System.out.println("				      `-.__.-'     ");
+	*/
+	
 }
