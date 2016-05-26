0f7f13095dcd48969e8c95dbc61b6e755401044d
unknown <Jack Silcock>
2016-05-26 16:12:42 +0000

    Commit mongo connection

--- workspace/NBGardens/src/main/mongoConnect.java
+++ workspace/NBGardens/src/main/mongoConnect.java
@@ -1,39 +1,68 @@
 package main;
 
-import com.mongodb.*;
+import com.mongodb.BasicDBObject;
+import com.mongodb.BulkWriteOperation;
+import com.mongodb.BulkWriteResult;
+import com.mongodb.Cursor;
+import com.mongodb.DB;
+import com.mongodb.DBCollection;
+import com.mongodb.DBCursor;
+import com.mongodb.DBObject;
+import com.mongodb.MongoClient;
+import com.mongodb.ParallelScanOptions;
+import com.mongodb.ServerAddress;
 
+
+import java.util.List;
+import java.util.Set;
+
+import static java.util.concurrent.TimeUnit.SECONDS;
 
 
 public class mongoConnect {
 
+	public static void main(String[] args) {
 	
-	MongoClient mongoClient = new MongoClient( "localhost" , 27017 );
+		  try {
 
-	
-	
-	
-	
-	
-	
-	/*
-	
-	System.out.println("			   	        _._                             ");                     
-	System.out.println("                                   _.-``__ ''-._                   ");                          
-	System.out.println("                              _.-``    `.  `_.  ''-._           ");
-	System.out.println("			  .-`` .-```.  ```/    _.,_ ''-._         ");                          
-	System.out.println("			 (    '      ,       .-`  | `,    )     ");
-	System.out.println("			 |`-._`-...-` __...-.``-._|'` _.-'|     ");
-	System.out.println("			 |    `-._   `._    /     _.-'    |     ");
-	System.out.println("			  `-._    `-._  `-./  _.-'    _.-'                    ");               
-	System.out.println("			 |`-._`-._    `-.__.-'    _.-'_.-'|                ");                  
-	System.out.println("			 |    `-._`-._        _.-'_.-'    |             ");      
-	System.out.println("			  `-._    `-._`-.__.-'_.-'    _.-'          ");                        
-	System.out.println("			 |`-._`-._    `-.__.-'    _.-'_.-'|       ");                           
-	System.out.println("			 |    `-._`-._        _.-'_.-'    |    ");                              
-	System.out.println("			  `-._    `-._`-.__.-'_.-'    _.-'  ");                                 
-	System.out.println("	                      `-._    `-.__.-'    _.-'   ");                                    
-	System.out.println("				  `-._        _.-'    ");                                       
-	System.out.println("				      `-.__.-'     ");
-	*/
-	
+				/**** Connect to MongoDB ****/
+				// Since 2.10.0, uses MongoClient
+				MongoClient mongo = new MongoClient(new ServerAddress("192.168.1.149", 27017));
+
+				/**** Get database ****/
+				// if database doesn't exists, MongoDB will create it for you
+				DB db = mongo.getDB("product");
+				
+				System.out.println("Connected to " + db);
+				
+				
+				
+				List<String> dbs = mongo.getDatabaseNames();
+				for(String ss : dbs){
+					System.out.println(ss);
+					
+					
+					DBCollection table = db.getCollection("product");
+
+					BasicDBObject searchQuery = new BasicDBObject();
+					searchQuery.put("description", "Zombie Gnome");
+
+					DBCursor cursor = table.find(searchQuery);
+
+					while (cursor.hasNext()) {
+						System.out.println(cursor.next());
+				}
+				
+				
+				
+				
+				}
+				
+				
+				
+				
+		  } catch (Exception e) {
+				e.printStackTrace();
+}
+	}
 }
